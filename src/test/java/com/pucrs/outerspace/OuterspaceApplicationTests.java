7353229c6bc0e64561d7433be5e81b950fd516e6
Lucas Piatnicki <lucas.piatnicki@edu.pucrs.br>
2020-07-17 18:15:16 -0300

    First commit

---
+++ src/test/java/com/pucrs/outerspace/OuterspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucrs.outerspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuterspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
